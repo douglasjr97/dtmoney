7e4849f2f3481c35d8ccad89982b29bcd704ab25
Douglas Falcao <falcao.douglas97@gmail.com>
2021-08-04 14:26:11 -0300

    0.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
@@ -11,11 +12,12 @@ import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <>
-    <div className="App">
- 
+    
+    <Header/>
+    <Dashboard/>
   <GlobalStyle/>
-  <Header/>
-    </div>
+ 
+   
     </>
   );
 }
